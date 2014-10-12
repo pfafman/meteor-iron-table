f9ad2f9ba3bd0581eb74177fc38537d94f6b76fe
Timothy Pfafman <tim@pfafman.com>
2014-10-12 09:59:19 -0700

    only show pager if number of records larger than increment

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Paging Table for IronRouter and Meteor',
-  version: "0.5.21",
+  version: "0.5.22",
   git: "https://github.com/pfafman/meteor-iron-table.git"
 });
 
