37d2bf2abb3f11db9a82544161d67a851f6d952f
Timothy Pfafman <tim@pfafman.com>
2014-10-14 09:59:12 -0700

    switch overflow to auto

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Paging Table for IronRouter and Meteor',
-  version: "0.5.23",
+  version: "0.5.24",
   git: "https://github.com/pfafman/meteor-iron-table.git"
 });
 
