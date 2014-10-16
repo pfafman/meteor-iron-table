5df0d3049e21abfe75373b8d74e56be0f293800e
Timothy Pfafman <tim@pfafman.com>
2014-10-16 09:35:29 -0700

    no margin on table

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Paging Table for IronRouter and Meteor',
-  version: "0.5.24",
+  version: "0.5.25",
   git: "https://github.com/pfafman/meteor-iron-table.git"
 });
 
