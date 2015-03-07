1c2cdadd4fe910fbb880660bad3e3b8a0f168ba8
Timothy Pfafman <tim@pfafman.com>
2015-03-07 09:18:09 -0800

    real value for hidden

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Paging Table for IronRouter and Meteor',
-  version: "0.7.5_2",
+  version: "0.7.5_3",
   git: "https://github.com/pfafman/meteor-iron-table.git"
 });
 
